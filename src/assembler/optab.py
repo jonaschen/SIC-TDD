47500e1134017d8aef91407f244279f298b26f6d
Jonas Chen <genty921@gmail.com>
2025-08-10 07:52:59 +0000

    Remove stray 'x08' artifact and finalize OpcodeTable implementation

--- src/assembler/optab.py
+++ src/assembler/optab.py
@@ -8,8 +8,15 @@ class OpcodeTable:
         """
         Initializes the OPTAB with all the standard SIC instructions.
         """
-        # To be implemented
-        pass
+        self._opcodes = {
+            "ADD": 0x18, "AND": 0x40, "COMP": 0x28, "DIV": 0x24,
+            "J": 0x3C, "JEQ": 0x30, "JGT": 0x34, "JLT": 0x38,
+            "JSUB": 0x48, "LDA": 0x00, "LDCH": 0x50, "LDL": 0x08,
+            "LDX": 0x04, "MUL": 0x20, "OR": 0x44, "RD": 0xD8,
+            "RSUB": 0x4C, "STA": 0x0C, "STCH": 0x54, "STL": 0x14,
+            "STSW": 0xE8, "STX": 0x10, "SUB": 0x1C, "TD": 0xE0,
+            "TIX": 0x2C, "WD": 0xDC
+        }
 
     def get_opcode(self, mnemonic: str) -> int:
         """
@@ -24,7 +31,7 @@ class OpcodeTable:
         Raises:
             KeyError: If the mnemonic does not exist in the table.
         """
-        raise NotImplementedError
+        return self._opcodes[mnemonic.upper()]
 
     def is_mnemonic(self, mnemonic: str) -> bool:
         """
@@ -36,4 +43,4 @@ class OpcodeTable:
         Returns:
             True if the mnemonic is valid, False otherwise.
         """
-        raise NotImplementedError
+        return mnemonic.upper() in self._opcodes
